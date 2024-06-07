0553eebe3df92ba3a5e3fe7b3f68a5d6bddecef5
Bhangdongho <bhang97@naver.com>
2024-06-07 21:20:38 +0900

    poject end

--- random-user.js
+++ random-user.js
@@ -6,13 +6,22 @@ const { faker } = require("@faker-js/faker");
 // localhost:5555/fake/users
 // req로 숫자를 받아서, 그 수 만큰 사용자 정보를 생성해주는 api
 
-app.get("/fake/users", function (req, res) {
-  res.status(200).json({
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-    fullName: faker.person.fullName(),
-    contact: faker.phone.number(),
-  });
+app.get("/fake/users/", function (req, res) {
+  const { num } = req.query;
+
+  let index = 1;
+  let users = [];
+  while (index <= num) {
+    //
+    users.push({
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      fullName: faker.person.fullName(),
+      contact: faker.phone.number(),
+    });
+    index++;
+  }
+  res.status(200).json(users);
 });
 
 app.listen(5555);
